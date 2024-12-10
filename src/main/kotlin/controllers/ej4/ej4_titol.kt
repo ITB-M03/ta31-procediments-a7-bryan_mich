3aa446810ef2f132358e3ae89d43a26a506dbd6e
Bryancc97 <bryan.carrera.7e8@itb.cat>
2024-12-10 12:55:24 +0100

    ej 4 codigo

--- src/main/kotlin/controllers/ej4/ej4_titol.kt
+++ src/main/kotlin/controllers/ej4/ej4_titol.kt
@@ -1,9 +1,12 @@
 package controllers.ej4
+import java.util.*
 
+fun main(){
+    val scanner = abrirScanner()
+    titol("Texto centrado")
 
-
-
-
+    cerrarScanner(scanner)
+}
 
 /*FUNCION*/
 fun titol(text: String) {
@@ -18,4 +21,32 @@ fun titol(text: String) {
 
     // Muestra el texto centrado
     println(" ".repeat(espais) + text)
+}
+/**
+ * Abrir Scanner
+ * @author Bryan
+ * @version 1.2
+ * @since 10/12/2024
+ *
+ *
+ * @param scanner abrimos el scanner
+ * @return scanner
+ */
+
+fun abrirScanner(): Scanner{
+    val scanner = Scanner(System.`in`)
+    return scanner
+}
+
+/**
+ * Cerrar Scanner
+ * @author Bryan
+ * @version 1.2
+ * @since 10/12/2024
+ *
+ * @param scanner cerramos el scanner
+ */
+
+fun cerrarScanner(scanner: Scanner){
+    scanner.close()
 }
